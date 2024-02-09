6fb2ceacab8f244d2b7231265b3e14f88b924f60
AnasAz07 <aazaroual20745@iesjoanramis.org>
2024-02-09 09:11:41 +0100

    Ex4 Polisses +

---
+++ MER4 Inserts.sql
@@ -0,0 +1,5 @@
+use MER4;
+INSERT INTO Polissa (Num_Polissa, Preu_Anual, Tipus_Pagament, Data_Contractació, Data_Inici_Vigència, Data_Final_Vigència, Client_DNI, Venedor_ID, Tipus_Polissa_ID)
+VALUES ('20', '212,89€', 'anual', '30-05-2023', '05-06-2023', '05-06-2030', '4150060X', '4', '4')
+
+
