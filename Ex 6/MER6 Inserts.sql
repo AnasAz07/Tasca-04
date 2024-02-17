06f0c5a43a5f05e41dc91827678bf91df8051669
AnasAz07 <aazaroual20745@iesjoanramis.org>
2024-02-17 17:32:43 +0100

    Ex 6 completo

--- Ex 6/MER6 Inserts.sql
+++ Ex 6/MER6 Inserts.sql
@@ -1,3 +1,3 @@
 USE MER6;
 INSERT INTO Reserves (ID, Data, Import, Num_Seient, Passatgers_ID, Vol_ID)
-VALUES ('1', '2024-01-23', '300€', '12', '2','3')
+VALUES ('15', '2029-08-09', '169€', '226', '4','1')
