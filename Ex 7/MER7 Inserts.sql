d737621b4745a2043060b708cf2be926728a03d1
AnasAz07 <aazaroual20745@iesjoanramis.org>
2024-02-19 19:15:49 +0100

    Ex 7 terminado

--- Ex 7/MER7 Inserts.sql
+++ Ex 7/MER7 Inserts.sql
@@ -1,3 +1,3 @@
 USE MER7;
-INSERT INTO Ticket (Numero, Num_Factura, Nom_Empresa, NIF, Domicili_Fiscal, Telefon, Web, Data, Forma_Pagament, Treballador_ID, Client_ID)
-VALUES ('3', '3', 'Mercadona', '19834735Z', 'España', '987465123', 'www.mercadona.com', '2024-01-04', 'Tarjeta Crèdit', '2', '4')
+INSERT INTO Linia_Ticket (ID, Quantitat, Descompte, Ticket_Numero, Producte_ID)
+VALUES ('15', '4', '0%', '5', '4')
