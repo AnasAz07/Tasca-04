e1e95701316e72c0803ae2381ffdd0a16d4823d4
AnasAz07 <aazaroual20745@iesjoanramis.org>
2024-02-19 12:02:26 +0100

    Ex 7 empezado

---
+++ Ex 7/MER7 Inserts.sql
@@ -0,0 +1,3 @@
+USE MER7;
+INSERT INTO Ticket (Numero, Num_Factura, Nom_Empresa, NIF, Domicili_Fiscal, Telefon, Web, Data, Forma_Pagament, Treballador_ID, Client_ID)
+VALUES ('3', '3', 'Mercadona', '19834735Z', 'España', '987465123', 'www.mercadona.com', '2024-01-04', 'Tarjeta Crèdit', '2', '4')
